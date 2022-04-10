03660e52cc64cbd373576f9638a4586436944d78
AjdiNNN <ajdinhukic007@gmail.com>
2022-04-10 03:16:53 +0200

    Modular chosing cities, transforim in new activity

--- app/src/main/java/com/example/airquality/MainActivity.java
+++ app/src/main/java/com/example/airquality/MainActivity.java
@@ -2,13 +2,35 @@ package com.example.airquality;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.Window;
+import android.widget.AdapterView;
+import android.widget.ListView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
+        setTitle("Chose City");
+        //* *EDIT* *
+        ListView listview = (ListView) findViewById(R.id.listView1);
+        listview.setOnItemClickListener(this);
+    }
+
+    public void onItemClick(AdapterView<?> l, View v, int position, long id) {
+        Log.i("HelloListView", "You clicked Item: " + id + " at position:" + position);
+        // Then you start a new Activity via Intent
+        Intent intent = new Intent();
+        intent.setClass(this, ListItemDetail.class);
+        intent.putExtra("position", position);
+        // Or / And
+        intent.putExtra("id", id);
+        startActivity(intent);
     }
 }
